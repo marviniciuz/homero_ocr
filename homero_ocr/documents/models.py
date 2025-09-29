e0e2333c0669525329ec8d81651940de8eec4986
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2025-09-29 15:55:55 -0300

    ajuste no models

--- homero_ocr/documents/models.py
+++ homero_ocr/documents/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Document(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    original_file = models.FileField(upload_to="uploads/")
+    processed_text = models.TextField(blank=True, null=True)
+    audio_file = models.FileField(upload_to="audios/", blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+ 
+    STATUS_CHOICES = [
+        ("pending", "Pendente"),
+        ("processing", "Processando"),
+        ("done", "Concluído"),
+        ("error", "Erro"),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+
+    def __str__(self):
+        return f"{self.original_file.name} ({self.status})"
